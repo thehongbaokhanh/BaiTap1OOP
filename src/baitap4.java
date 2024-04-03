f057bc14f11957d653480b1f3a4e144c8400ad3c
Your Name <thehongbaokhanh2005@gmail.com>
2024-04-03 14:36:11 +0700

    Add

--- src/baitap4.java
+++ src/baitap4.java
@@ -1,2 +1,77 @@
+import java.util.Scanner;
+
 public class baitap4 {
+    public static void main(String[] args) {
+        Scanner infor = new Scanner(System.in);
+        System.out.println("Hãy nhập số mà bạn muốn đọc thành chữ vào đây: ");
+        int number = infor.nextInt();
+        if (number <= 20 && number >= 0 ){
+            switch (number){
+                case 0:
+                    System.out.println("zero");
+                    break;
+                case 1:
+                    System.out.println("one");
+                    break;
+                case 2:
+                    System.out.println("two");
+                    break;
+                case 3:
+                    System.out.println("three");
+                    break;
+                case 4:
+                    System.out.println("four");
+                    break;
+                case 5:
+                    System.out.println("five");
+                    break;
+                case 6:
+                    System.out.println("six");
+                    break;
+                case 7:
+                    System.out.println("seven");
+                    break;
+                case 8:
+                    System.out.println("eight");
+                    break;
+                case 9:
+                    System.out.println("nine");
+                    break;
+                case 10:
+                    System.out.println("ten");
+                    break;
+                case 11:
+                    System.out.println("eleven");
+                    break;
+                case 12:
+                    System.out.println("twelve");
+                    break;
+                case 13:
+                    System.out.println("thirteen");
+                    break;
+                case 14:
+                    System.out.println("fourteen");
+                    break;
+                case 15:
+                    System.out.println("fifteen");
+                    break;
+                case 16:
+                    System.out.println("sixteen");
+                    break;
+                case 17:
+                    System.out.println("seventeen");
+                    break;
+                case 18:
+                    System.out.println("eighteen");
+                    break;
+                case 19:
+                    System.out.println("nineteen");
+                    break;
+                case 20:
+                    System.out.println("twenty");
+                    break;
+
+            }
+        }
+    }
 }
